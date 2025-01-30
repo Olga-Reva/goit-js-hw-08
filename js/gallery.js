bf0ab3a1a6a6684de13f5e181afe0ca33814d775
Reva Olga <Reva.Olga.Vladimirovna@gmail.com>
2025-01-31 01:51:32 +0200

    Updating the image gallery

--- js/gallery.js
+++ js/gallery.js
@@ -25,20 +25,18 @@ const images = [
 const galleryContainer = document.querySelector(".gallery");
 
 const galleryMarkup = images
-  .map(
-    ({ preview, original, description }) => `
-      <li class="gallery-item">
-        <a class="gallery-link" href="${original}">
-          <img
-            class="gallery-image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </li>
-    `
-  )
+  .map(({ preview, original, description }) => (
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  ))
   .join("");
 
 galleryContainer.innerHTML = galleryMarkup;
